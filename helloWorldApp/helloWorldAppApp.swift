4da410a897cfa6da9a825761e2dfe1fca3594c82
andres <andresriosaragon@gmail.com>
2026-07-06 01:17:26 -0700

    Initial Commit

---
+++ helloWorldApp/helloWorldAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloWorldAppApp.swift
+//  helloWorldApp
+//
+//  Created by user947756 on 7/6/26.
+//
+
+import SwiftUI
+
+@main
+struct helloWorldAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
